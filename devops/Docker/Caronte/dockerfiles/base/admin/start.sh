9842b3a60a42eaa725ecf56ce929da82c98ee8f3
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-11-05 12:20:56 +0100

    PROYECTO BASE: IMAGEN BASE

--- devops/Docker/Caronte/dockerfiles/base/admin/start.sh
+++ devops/Docker/Caronte/dockerfiles/base/admin/start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-#Encargada de dejar el contenedor vivo en background
-tail -f /dev/null 
+
+newUser(){
+    useradd -rm -d /home/pedro -s /bin/bash pedro
+    echo "pedro:1234" | chpasswd
+    echo "Bienvenido Pedro ..." > /home/pedro/bienvenida.txt
+}
+
+main(){
+    newUser
+    tail -f /dev/null #Encargada de dejar el contenedor vivo en background
+}
+
+main
