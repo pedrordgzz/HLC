4f7f32ce798ee02fd7466cd9b7b98c12f548fc9b
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-11-11 13:30:36 +0100

    check-usuario

--- devops/Docker/Caronte/dockerfiles/base/admin/start.sh
+++ devops/Docker/Caronte/dockerfiles/base/admin/start.sh
@@ -2,13 +2,29 @@
 
 set -e #Carga las variables de entorno, pasadas desde el docker-compose.
 
+check-usuario(){
+    if grep -q ${USUARIO} /etc/passwd; 
+    then
+        echo "El usuario ${USUARIO} ya existe." >> /root/logs/informe.log
+        return 1
+    else
+         echo "El usuario ${USUARIO} no existe." >> /root/logs/informe.log
+        return 0
+    fi
+}
+
 newUser(){
+    check-usuario
+    if [ $? -eq 0 ]; 
+        then
     useradd -rm -d /home/${USUARIO} -s /bin/bash ${USUARIO}
     echo "${USUARIO}:${PASSWORD}" | chpasswd
     echo "Bienvenido ${USUARIO} ..." > /home/${USUARIO}/bienvenida.txt
+
 }
 
 main(){
+    touch /root/logs/informe.log
     newUser
     tail -f /dev/null #Encargada de dejar el contenedor vivo en background
 }
