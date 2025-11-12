15adf8767fe390c90c963eaccc323247df3023b5
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-11-12 11:53:20 +0100

    creacion usuario

--- devops/Docker/Caronte/dockerfiles/base/admin/start.sh
+++ devops/Docker/Caronte/dockerfiles/base/admin/start.sh
@@ -1,46 +1,7 @@
 #!/bin/bash
 
 set -e #Carga las variables de entorno, pasadas desde el docker-compose.
-
-check-usuario(){
-    if grep -q ${USUARIO} /etc/passwd; 
-    then
-        echo "El usuario ${USUARIO} ya existe." >> /root/logs/informe.log
-        return 1
-    else
-         echo "El usuario ${USUARIO} no existe." >> /root/logs/informe.log
-        return 0
-    fi
-}
-
-check-home(){
-    if [ ! -d "/home/${USUARIO}" ]; 
-    then
-        echo "El directorio /home/${USUARIO} no existe." >> /root/logs/informe.log
-        return 0 #true
-    else
-         echo "El directorio /home/${USUARIO} ya existe." >> /root/logs/informe.log
-        return 1 #false
-    fi
-}
-
-newUser(){
-    check-usuario
-    if [ $? -eq 0 ]; 
-        then
-            check-home
-            if [ $? -eq 0 ];
-                then
-                    useradd -rm -d /home/${USUARIO} -s /bin/bash ${USUARIO}
-                    echo "${USUARIO}:${PASSWORD}" | chpasswd
-                    echo "Usuario ${USUARIO} creado" > /home/${USUARIO}/bienvenida.txt
-                else   
-                    echo "Usuario ${USUARIO} no creado, pero /home/${USUARIO} existente" > /home/${USUARIO}/bienvenida.txt         
-            fi
-        else
-            echo "No se crea el usuario ${USUARIO} porque ya existe" >> /root/logs/informe.log
-    fi
-}
+source /root/admin/usuarios/mainusers.sh
 
 main(){
     touch /root/logs/informe.log
