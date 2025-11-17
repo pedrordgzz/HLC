25a7c1abdbf588bb8f36f634e4134d7aba9d3d6d
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-11-17 19:51:34 +0100

    prueba

--- devops/Docker/Caronte/dockerfiles/base/admin/ssh/main-ssh.sh
+++ devops/Docker/Caronte/dockerfiles/base/admin/ssh/main-ssh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 configurar-ssh() {
-    sed -i 's/#Port 22/Port 45678/' /etc/ssh/sshd_config
+    sed -i 's/#Port 22/Port '$PORT_SSH'/' /etc/ssh/sshd_config
     sed -i 's/#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
     service ssh restart
 }
