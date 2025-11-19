6bbf55981806f190b9e73b57794752344d758f76
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-11-19 11:01:15 +0100

    .

--- devops/Docker/Caronte/dockerfiles/base/admin/ssh/main-ssh.sh
+++ devops/Docker/Caronte/dockerfiles/base/admin/ssh/main-ssh.sh
@@ -3,6 +3,8 @@ configurar-ssh() {
     sed -i 's/#Port 22/Port '$PORT_SSH'/' /etc/ssh/sshd_config
     sed -i 's/#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
     service ssh restart
+    mkdir /home/$USUARIO/.ssh
+    cp /root/admin/common/* /home/$USUARIO/.ssh/
     #exec /usr/sbin/sshd -D &
 }
 
