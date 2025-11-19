2533676798787ae0928e21711fde78c3802944fd
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-11-19 10:52:35 +0100

    authorized keys

--- devops/Docker/Caronte/dockerfiles/base/admin/ssh/main-ssh.sh
+++ devops/Docker/Caronte/dockerfiles/base/admin/ssh/main-ssh.sh
@@ -2,7 +2,7 @@
 configurar-ssh() {
     sed -i 's/#Port 22/Port '$PORT_SSH'/' /etc/ssh/sshd_config
     sed -i 's/#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
-    mkdir /home/${USUARIO}/.ssh
+    mkdir /home/${USUARIO}/.ssh/authorized_keys
     service ssh restart
     #exec /usr/sbin/sshd -D &
 }
