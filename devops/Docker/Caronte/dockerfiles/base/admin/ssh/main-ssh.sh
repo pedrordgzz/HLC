b4f21bb43204293a90aae1a1208ce63560490826
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-11-26 12:14:05 +0100

    prueba

--- devops/Docker/Caronte/dockerfiles/base/admin/ssh/main-ssh.sh
+++ devops/Docker/Caronte/dockerfiles/base/admin/ssh/main-ssh.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
+
 configurar-ssh() {
-    sed -i 's/#Port 22/Port '$PORT_SSH'/' /etc/ssh/sshd_config
+    sed -i "s/#Port 22/Port $PORT_SSH/" /etc/ssh/sshd_config
     sed -i 's/#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
 
-    if [ ! -d /home/$USUARIO/.ssh]
-    then
-    mkdir /home/$USUARIO/.ssh
-    #cp /root/admin/common/* /home/$USUARIO/.ssh/
-    cat /root/admin/common/id_ed25519.pub > /home/$USUARIO/.ssh/authorized_keys
+    if [ ! -d "/home/$USUARIO/.ssh" ]; then
+        mkdir -p "/home/$USUARIO/.ssh"
+        if [ -f /root/admin/common/id_ed25519.pub ]; then
+            cat /root/admin/common/id_ed25519.pub > "/home/$USUARIO/.ssh/authorized_keys"
+        fi
+        chmod 700 "/home/$USUARIO/.ssh"
+        chmod 600 "/home/$USUARIO/.ssh/authorized_keys"
+        chown -R "$USUARIO:$USUARIO" "/home/$USUARIO/.ssh"
     fi
-
     exec /usr/sbin/sshd
-
 }
 
 configurar-sudo() {
     if [ -f /etc/sudoers ]; then
-        echo "$USUARIO ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+        if ! grep -q "$USUARIO ALL=(ALL) NOPASSWD:ALL" /etc/sudoers; then
+            echo "$USUARIO ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+        fi
     fi
 }
