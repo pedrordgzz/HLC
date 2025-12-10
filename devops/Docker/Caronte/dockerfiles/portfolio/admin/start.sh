8b22b1ecdfc1cded608de1e8108d5f74e4db7ec3
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-12-10 12:13:52 +0100

    UBNGINX configurada y portfolio con variables

--- devops/Docker/Caronte/dockerfiles/portfolio/admin/start.sh
+++ devops/Docker/Caronte/dockerfiles/portfolio/admin/start.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
-
 load_entrypoint_nginx(){
     # Asumo que esto configura cosas, pero no arranca el servicio o se detiene
     bash /root/admin/sweb/nginx/admin/start.sh
 }
 
 iniciar_nginx(){
-    # CAMBIO IMPORTANTE: Usamos 'start' o 'restart', no 'reload'
-    # Si nginx no está corriendo, reload falla y no hace nada.
     service nginx restart
+    nginx
 }
 
 main(){
     load_entrypoint_nginx
-    
-    # Aseguramos que Nginx arranque explícitamente
     iniciar_nginx
-    
-    # Mantenemos el contenedor vivo
-    tail -f /dev/null
 }
 
 main
