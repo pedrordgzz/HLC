651398b5119da86eeaaf1bcebad3fed5180982c8
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-12-15 09:33:23 +0100

    a

--- devops/Docker/Caronte/dockerfiles/pokeapi/admin/start.sh
+++ devops/Docker/Caronte/dockerfiles/pokeapi/admin/start.sh
@@ -4,14 +4,9 @@ load_entrypoint_nginx(){
     bash /root/admin/sweb/nginx/admin/start.sh
 }
 
-react_admin(){
-    npm i  
-    npm run build
-}
-
 main(){
     load_entrypoint_nginx
-    react_admin
+
 }
 
 main
