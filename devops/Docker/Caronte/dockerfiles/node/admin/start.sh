e465d9fccfca5fc9f39092bb3f444f9cb35bd390
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-12-15 17:25:00 +0100

    modif

---
+++ devops/Docker/Caronte/dockerfiles/node/admin/start.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e 
+
+load_entrypoint_nginx(){
+    bash /root/admin/nginx/admin/start.sh
+}
+
+workdir(){
+    cd /root/admin/node/proyectos/pmegacrossover
+}
+
+dependencias(){
+    npm install
+    npm run build
+    cp -r dist/* /var/www/html/
+}
+
+
+nginxreload(){
+    nginx -t
+    service nginx reload
+}
+
+
+main(){
+    load_entrypoint_nginx
+    workdir
+    dependencias
+    nginxreload
+    tail -f /dev/null
+}
+
+main
