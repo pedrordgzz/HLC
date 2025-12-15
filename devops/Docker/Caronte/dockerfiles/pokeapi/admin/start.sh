3f36c48a3099f1d28d5d78b7f2f325351b7cbec0
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-12-15 17:14:39 +0100

    df

--- devops/Docker/Caronte/dockerfiles/pokeapi/admin/start.sh
+++ devops/Docker/Caronte/dockerfiles/pokeapi/admin/start.sh
@@ -1,51 +1,34 @@
 #!/bin/bash
-set -e
 
-compile_and_deploy(){
-    # Usamos la variable de entorno que definiste en el .env
-    PROJECT_DIR="/root/admin/base/$PROYECTO"
-    
-    echo "--> Iniciando compilación de $PROYECTO en $PROJECT_DIR..."
-    
-    if [ -d "$PROJECT_DIR" ]; then
-        cd "$PROJECT_DIR"
-        
-        # 1. Instalamos dependencias y compilamos
-        echo "--> Instalando dependencias (npm install)..."
-        npm install
-        
-        echo "--> Compilando proyecto (npm run build)..."
-        npm run build
-        
-        # 2. Movemos el resultado (dist) a donde Nginx lo lee
-        # Nota: Vite/React suelen crear la carpeta 'dist' o 'build'
-        if [ -d "dist" ]; then
-            echo "--> Desplegando a /var/www/html..."
-            rm -rf /var/www/html/*
-            cp -r dist/* /var/www/html/
-        elif [ -d "build" ]; then
-             # Por si tu proyecto crea carpeta 'build' en vez de 'dist'
-            echo "--> Desplegando a /var/www/html..."
-            rm -rf /var/www/html/*
-            cp -r build/* /var/www/html/
-        fi
-        
-        # 3. Permisos
-        chown -R www-data:www-data /var/www/html
-    else
-        echo "--> ERROR: No encuentro el código en $PROJECT_DIR"
-    fi
-}
+set -e 
 
 load_entrypoint_nginx(){
-    # Arranca Nginx (daemon off)
-    bash /root/admin/sweb/nginx/admin/start.sh
-    service nginx start
+    bash /root/admin/nginx/admin/start.sh
 }
 
+workdir(){
+    cd /root/admin/node/proyectos/pmegacrossover
+}
+
+dependencias(){
+    npm install
+    npm run build
+    cp -r dist/* /var/www/html/
+}
+
+
+nginxreload(){
+    nginx -t
+    service nginx reload
+}
+
+
 main(){
-    compile_and_deploy
     load_entrypoint_nginx
+    workdir
+    dependencias
+    nginxreload
+    tail -f /dev/null
 }
 
 main
