c6c7e7ac87ff8ab5333c5f0eafa3d41f9c886e46
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-12-16 19:49:00 +0100

    pruebas

--- devops/Docker/Caronte/dockerfiles/preact/admin/start.sh
+++ devops/Docker/Caronte/dockerfiles/preact/admin/start.sh
@@ -3,32 +3,37 @@
 set -e 
 
 load_entrypoint_nginx(){
-    bash /root/admin/nginx/start.sh
-}
-
-workdir(){
-    cd /root/admin/node/proyectos/pmegacrossover
+    bash /root/admin/node/start.sh
 }
 
 dependencias(){
-    npm install
-    npm run build
-    cp -r dist/* /var/www/html/
+  npm run build
+  rm -rf /var/www/html/*
+        if [ -d "dist" ]; then
+            cp -r dist/. /var/www/html/
+        elif [ -d "build" ]; then
+             cp -r build/. /var/www/html/
+        fi
+chown -R www-data:www-data /var/www/html
 }
 
 
 nginxreload(){
-    nginx -t
-    service nginx reload
+    if nginx -t; then
+       nginx -g 'daemon off;'
+    else
+        echo "ERROR: Configuración Nginx inválida" >> /root/logs/informe_pokeapi.log
+        exit 1
+    fi
 }
+
 
 
 main(){
     load_entrypoint_nginx
-    workdir
     dependencias
     nginxreload
-    tail -f /dev/null
+
 }
 
 main
