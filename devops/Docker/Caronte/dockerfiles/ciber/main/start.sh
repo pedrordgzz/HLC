d25698af6426fbe739dc016421d1b1cd633f4dc4
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-12-16 17:58:14 +0100

    capa ciber

---
+++ devops/Docker/Caronte/dockerfiles/ciber/main/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REPORT_FILE="/output/auditoria_completa.txt"
+
+echo "   INFORME DE CIBERSEGURIDAD AUTOMATIZADO" >> $REPORT_FILE
+echo "   Fecha: $(date)" >> $REPORT_FILE
+
+echo "[+] Iniciando Fase 1: Mapeo de Puertos (Nmap)..."
+echo -e "\n--- RESULTADOS NMAP ---" >> $REPORT_FILE
+nmap -Pn -sV ubbase >> $REPORT_FILE
+echo "   AUDITORÍA FINALIZADA" >> $REPORT_FILE
+
+echo "[OK] Reporte generado en $REPORT_FILE"
