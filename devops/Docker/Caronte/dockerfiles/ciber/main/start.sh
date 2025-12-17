0af818fbf7ddd40165c462c1e9d2b0bdb496ec56
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-12-17 09:00:59 +0100

    mejora final

--- devops/Docker/Caronte/dockerfiles/ciber/main/start.sh
+++ devops/Docker/Caronte/dockerfiles/ciber/main/start.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 set -e
-
-# Definimos la ubicación por defecto si la variable no viene del env
-REPORT_FILE=${REPORT_FILE:-/output/auditoria_completa.txt}
-
 auditoria(){
-    echo "   INFORME DE CIBERSEGURIDAD AUTOMATIZADO" > "$REPORT_FILE" # > Sobrescribe, >> Añade
+    echo "   INFORME DE CIBERSEGURIDAD AUTOMATIZADO" > "$REPORT_FILE" 
     echo "   Fecha: $(date)" >> "$REPORT_FILE"
     echo "[+] Iniciando Fase 1: Mapeo de Puertos (Nmap)..."
-    
+
     echo -e "\n--- RESULTADOS NMAP (Localhost) ---" >> "$REPORT_FILE"
-    # Escaneamos localhost para ver qué puertos tiene abiertos este contenedor
-    nmap -Pn -sV 127.0.0.1 >> "$REPORT_FILE"
+    nmap -Pn -sV http://194.163.138.9/ >> "$REPORT_FILE"
     
     echo "   AUDITORÍA FINALIZADA" >> "$REPORT_FILE"
     echo "[OK] Reporte generado en $REPORT_FILE"
@@ -19,16 +14,11 @@ auditoria(){
 
 load_entrypoint_base(){
     echo "[+] Iniciando servicios base..."
-    # Usamos exec para que el proceso base tome el PID 1 al final
-    # O simplemente lo llamamos si sabemos que se queda en foreground.
     bash /root/admin/base/start.sh
 }
 
 main(){
- # 1. Ejecutamos la auditoría PRIMERO
  auditoria
- 
- # 2. LUEGO cargamos el servicio base que dejará el contenedor corriendo
  load_entrypoint_base
 }
 
