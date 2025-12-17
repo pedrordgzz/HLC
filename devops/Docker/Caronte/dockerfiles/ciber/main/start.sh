6de1f1465df8c10fc2bbdddb29ad589d84a543f0
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-12-17 09:28:06 +0100

    modif ciber

--- devops/Docker/Caronte/dockerfiles/ciber/main/start.sh
+++ devops/Docker/Caronte/dockerfiles/ciber/main/start.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 set -e
+
 auditoria(){
+    # Esto significa: Usa $IP, pero si está vacía, usa 127.0.0.1
+    local OBJETIVO="${IP:-127.0.0.1}"
+
     echo "   INFORME DE CIBERSEGURIDAD AUTOMATIZADO" > "$REPORT_FILE" 
     echo "   Fecha: $(date)" >> "$REPORT_FILE"
+    echo "   Analizando IP: $OBJETIVO" >> "$REPORT_FILE"
     echo "[+] Iniciando Fase 1: Mapeo de Puertos (Nmap)..."
 
-    echo -e "\n--- RESULTADOS NMAP (Localhost) ---" >> "$REPORT_FILE"
-    nmap -Pn -sV http://127.0.0.1/ >> "$REPORT_FILE"
+    # Espera para que arranquen los servicios
+    sleep 10
+
+    echo -e "\n--- RESULTADOS NMAP ---" >> "$REPORT_FILE"
+    
+    nmap -Pn -sV "$OBJETIVO" >> "$REPORT_FILE"
     
     echo "   AUDITORÍA FINALIZADA" >> "$REPORT_FILE"
     echo "[OK] Reporte generado en $REPORT_FILE"
 }
 
 load_entrypoint_base(){
-    echo "[+] Iniciando servicios base..."
-    bash /root/admin/base/start.sh
+    bash /root/admin/base/start.sh &
 }
 
 main(){
- auditoria
  load_entrypoint_base
+ auditoria
 }
 
 main
