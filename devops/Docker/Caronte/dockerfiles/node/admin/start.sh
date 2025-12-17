0dac624a7089e6725e53b0c031a345ae16b4f591
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-12-17 08:34:46 +0100

    solucion start.sh

--- devops/Docker/Caronte/dockerfiles/node/admin/start.sh
+++ devops/Docker/Caronte/dockerfiles/node/admin/start.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 set -e
 
+
 deploy_app(){
+    # Verificamos si hay algo que copiar en el volumen
     if [ -d "$VOLUME_DIR" ] && [ "$(ls -A $VOLUME_DIR)" ]; then
-        echo "Cargando código desde volumen: $VOLUME_DIR"
-        # Hago uncp para actualizar archivos
-        cp -r $VOLUME_DIR/. $APP_DIR/
+        echo "Código existente en el volumen: $VOLUME_DIR"
+        mkdir -p "$APP_DIR"
+        echo "Copiando archivos"
+        cp -rf "$VOLUME_DIR/." "$APP_DIR/"
+        echo "Copia finalizada."
+    else
+        echo "Volumen vacío. Usando código preexistente en la imagen."
     fi
 
     cd "$APP_DIR"
-
 }
 
-load_entrypoint_base(){
-    bash /root/admin/sweb/nginx/admin/start.sh
+instalar_dependencias(){ 
+    if [ -f "package.json" ]; then
+        npm install
+        
+        echo "Instalación completada."
+    else
+        echo "No se encontró package.json. Saltando instalación."
+    fi
 }
+
 main(){
-     load_entrypoint_base
     deploy_app
+    instalar_dependencias
 }
 
 main
