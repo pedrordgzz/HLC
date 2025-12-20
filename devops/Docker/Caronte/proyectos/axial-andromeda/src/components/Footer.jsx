3055160d1baf89206d2a7685b849b7c5281d0097
Peedroorgzz <pedrojuanrodriguezz2005@gmail.com>
2025-12-20 22:00:27 +0100

    Modifiacion final al footer

--- devops/Docker/Caronte/proyectos/axial-andromeda/src/components/Footer.jsx
+++ devops/Docker/Caronte/proyectos/axial-andromeda/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer style={{ padding: '2rem', textAlign: 'center', background: 'var(--bg-primary)', borderTop: '1px solid rgba(255,0,127,0.2)' }}>
-            <p style={{ color: 'var(--text-secondary)' }}>© 2024 ASIR.dev - Portfolio Freelance.</p>
+            <p style={{ color: 'var(--text-secondary)' }}>2025 © Pedro Juan Rodríguez Giménez - Todos los derechos reservados.</p>
         </footer>
     );
 };
